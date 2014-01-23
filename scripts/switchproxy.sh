a19ae5a5d96d9c913946148f6a8c72f0eb45da19
Oliver Kohl <o.kohl@sap.com>
2014-01-23 17:20:17 +0100

    fixing some bash urks

--- scripts/switchproxy.sh
+++ scripts/switchproxy.sh
@@ -3,15 +3,15 @@
 # Switch proxy
 # Switches between SAP proxy config and no proxy config at all
 
-if [ -z "$ALL_PROXY"]; then
-  echo -n "SAP Proxy enabled!"
-  export ALL_PROXY="http://proxy.wdf.sap.corp:8080"
-  export HTTP_PROXY="http://proxy.wdf.sap.corp:8080"
-  export HTTPS_PROXY="http://proxy.wdf.sap.corp:8080"
-  export NO_PROXY=".sap.corp,localhost"
+if [ -z "$all_proxy" ]; then
+  echo "SAP Proxy enabled!"
+  export all_proxy="http://proxy.wdf.sap.corp:8080"
+  export http_proxy="http://proxy.wdf.sap.corp:8080"
+  export https_proxy="http://proxy.wdf.sap.corp:8080"
+  export no_proxy=".sap.corp,localhost"
 else
-  echo -n "SAP Proxy disabled!"
-  unset ALL_PROXY
-  unset HTTP_PROXY
-  unset HTTPS_PROXY
+  echo "SAP Proxy disabled!"
+  unset all_proxy
+  unset http_proxy
+  unset https_proxy
 fi
