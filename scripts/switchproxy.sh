f5dc10b59c7dc0fc8308cceffb9d8d06d02f3150
Oliver Kohl <o.kohl@sap.com>
2014-02-26 18:15:59 +0100

    fix for dot issue in proxy switcher

--- scripts/switchproxy.sh
+++ scripts/switchproxy.sh
@@ -8,7 +8,7 @@ if [ -z "$all_proxy" ]; then
   export all_proxy="http://proxy.wdf.sap.corp:8080"
   export http_proxy="http://proxy.wdf.sap.corp:8080"
   export https_proxy="http://proxy.wdf.sap.corp:8080"
-  export no_proxy=".sap.corp,localhost"
+  export no_proxy="sap.corp,localhost"
 else
   echo "SAP Proxy disabled!"
   unset all_proxy
